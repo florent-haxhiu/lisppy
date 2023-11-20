e3b85d5517691bc6a40da31152a3260105161430
florent-haxhiu <florenthaxhiu03@gmail.com>
2023-11-20 22:26:02 +0000

    Can be compiled on windows

--- prompt.c
+++ prompt.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Run this code on windows */
+#ifndef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline */
+char *readline(char *prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char *cpy = malloc(strlen(buffer) + 1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy) - 1] = '\0';
+  return cpy;
+}
+
+/* Fake add_history */
+void add_history(char *unused){};
+
+#else
 #include <editline/readline.h>
+#endif /* ifndef _WIN32 */
 
 int main(int argc, char **argv) {
 
@@ -10,7 +31,10 @@ int main(int argc, char **argv) {
   puts("Press Ctrl+C to Exit\n");
 
   while (1) {
-    /* Output prompt & get input */
+    /*
+     * Output prompt & get input
+     * Either way, readline will always be correct
+     */
     char *input = readline("\nlispy> ");
 
     add_history(input);
